f79b3313def4dda1c337bf544eaeb449bf936ee2
byl070 <61816285+byl070@users.noreply.github.com>
2025-06-06 11:51:33 +0800

    更新 connect.c

--- trunk/linux-4.4.x/fs/cifs/connect.c
+++ trunk/linux-4.4.x/fs/cifs/connect.c
@@ -18,6 +18,8 @@
  *   along with this library; if not, write to the Free Software
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
+	
+#include <linux/nls.h>
 #include <linux/fs.h>
 #include <linux/net.h>
 #include <linux/string.h>
